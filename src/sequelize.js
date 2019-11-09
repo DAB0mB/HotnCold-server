51d290990ae09f34375420c2f0bca455ddab9a0b
Eytan Manor <emanor6@gmail.com>
2019-11-09 11:59:58 +0200

    Add SSL option when initializing DB connection

--- src/sequelize.js
+++ src/sequelize.js
@@ -4,14 +4,21 @@ let sequelize;
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: process.env.NODE_ENV == 'production',
+    },
   });
-} else {
+}
+else {
   sequelize = new Sequelize(
     process.env.TEST_DATABASE || process.env.DATABASE,
     process.env.DATABASE_USER,
     process.env.DATABASE_PASSWORD || null,
     {
       dialect: 'postgres',
+      dialectOptions: {
+        ssl: process.env.NODE_ENV == 'production',
+      },
     },
   );
 }
